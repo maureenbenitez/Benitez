b450ed67c1bd96bb5669593a08db84217e362862
Maureen Benitez <124063783+maureenbenitez@users.noreply.github.com>
2023-05-25 11:30:47 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,27 +1,24 @@
 var slideshowContainers = document.getElementsByClassName("slideshow-container");
 
-for(let s = 0; s < slideshowContainers.length; s++) {
-            
-    var cycle = slideshowContainers[s].dataset.cycle;
-    
-    var slides = slideshowContainers[s].querySelectorAll('.mySlides');
-    var slideIndex = 0;
-    
-    showSlides(slides, slideIndex, cycle);
+for(let s = 0; s < slideshowContainers.length; s++) {          
+  var cycle = slideshowContainers[s].dataset.cycle;
+  var slides = slideshowContainers[s].querySelectorAll('.mySlides');
+  var slideIndex = 0;    
+  showSlides(slides, slideIndex, cycle);
 };
 
-
 function showSlides(slides, slideIndex, cycle) {
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    };
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    };
-    slides[slideIndex - 1].style.display = "block";
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  };
+  slideIndex++;
     
-    setTimeout(function() {
-        showSlides(slides, slideIndex, cycle)
-    }, cycle);
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  };
+  slides[slideIndex - 1].style.display = "block";
+    
+  setTimeout(function() {
+    showSlides(slides, slideIndex, cycle)
+  }, cycle);
 };
